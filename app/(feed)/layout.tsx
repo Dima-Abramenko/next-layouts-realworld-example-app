ff2badce44594a7470f27bb0a37a2c732cc5c18f
Dmitry Abramenko <dmitry.abramenko@madpaws.com.au>
2023-01-22 01:04:41 +0600

    feat: implement tags fetching from API

--- app/(feed)/layout.tsx
+++ app/(feed)/layout.tsx
@@ -1,30 +1,48 @@
 import type { ReactElement, ReactNode } from 'react';
 import React from 'react';
 
+import type { TagsResponse } from '~/common/types';
 import { Banner } from '~/components/home/Banner';
 import { FeedManager } from '~/components/home/FeedManager';
 import { TagsList } from '~/components/home/TagsList';
+
+// TODO: Complete data fetching
+const getTags = async (): Promise<TagsResponse> => {
+  // eslint-disable-next-line compat/compat
+  const response = await fetch(`${process.env.API_BASE_URL}/tags`, { cache: 'no-store' });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch tags');
+  }
+
+  return response.json();
+};
 
 type Props = {
   children: ReactNode;
 };
 
 // TODO: Complete FeedLayout
-const FeedLayout = ({ children }: Props): ReactElement => (
-  <div className="home-page">
-    <Banner />
-    <div className="container page">
-      <div className="row">
-        <div className="col-md-9">
-          <FeedManager />
-          {children}
-        </div>
-        <div className="col-md-3">
-          <TagsList />
+const FeedLayout = async ({ children }: Props): Promise<ReactElement> => {
+  const data = await getTags();
+  const { tags } = data;
+
+  return (
+    <div className="home-page">
+      <Banner />
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-9">
+            <FeedManager />
+            {children}
+          </div>
+          <div className="col-md-3">
+            <TagsList tags={tags} />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default FeedLayout;
--- src/components/home/TagsList.tsx
+++ src/components/home/TagsList.tsx
@@ -1,26 +1,28 @@
+import Link from 'next/link';
 import type { ReactElement } from 'react';
 import React from 'react';
 
+import { TAG_FEED_ROUTE } from '~/common/constants';
+import { replaceDynamicRoute } from '~/components/home/utils';
+
+type Props = {
+  tags: string[];
+};
+
 // TODO: Complete TagsList component
-export const TagsList = (): ReactElement => (
+export const TagsList = ({ tags }: Props): ReactElement => (
   <div className="sidebar">
     <p>Popular Tags</p>
     <div className="tag-list">
-      <a className="tag-pill tag-default" href="/">
-        programming
-      </a>
-      <a className="tag-pill tag-default" href="/">
-        javascript
-      </a>
-      <a className="tag-pill tag-default" href="/">
-        angularjs
-      </a>
-      <a className="tag-pill tag-default" href="/">
-        react
-      </a>
-      <a className="tag-pill tag-default" href="/">
-        node
-      </a>
+      {tags.map((tag) => (
+        <Link
+          className="tag-pill tag-default"
+          href={replaceDynamicRoute(TAG_FEED_ROUTE, 'tag', tag)}
+          key={tag}
+        >
+          {tag}
+        </Link>
+      ))}
     </div>
   </div>
 );
