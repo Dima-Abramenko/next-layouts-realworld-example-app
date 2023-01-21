29989ea79240643c933bf84877dec94f282aaca6
Dmitry Abramenko <dmitry.abramenko@madpaws.com.au>
2023-01-22 00:57:17 +0600

    feat: create draft pages for feed section

--- app/(feed)/feed/[tag]/page.tsx
+++ app/(feed)/feed/[tag]/page.tsx
@@ -1,31 +1,37 @@
 import type { FC } from 'react';
 
-// TODO: Complete YourFeed page
-const YourFeed: FC = () => (
+// TODO: Complete TagFeed page
+const TagFeed: FC = () => (
   <>
     <div className="article-preview">
       <div className="article-meta">
+        {/* TODO: replace with Link tag */}
         <a href="/">
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img alt="user avatar" src="http://i.imgur.com/N4VcUeJ.jpg" />
         </a>
         <div className="info">
+          {/* TODO: replace with Link tag */}
           <a className="author" href="/">
-            Albert Pai
+            Don Derri
           </a>
-          <span className="date">January 20th</span>
+          <span className="date">January 11th</span>
         </div>
         <button className="btn btn-outline-primary btn-sm pull-xs-right" type="button">
-          <i className="ion-heart"></i> 32
+          <i className="ion-heart"></i> 100
         </button>
       </div>
+      {/* TODO: replace with Link tag */}
       <a className="preview-link" href="/">
-        <h1>The song you will not ever stop singing. No matter how hard you try.</h1>
+        <h1>Some very very long title</h1>
         <p>This is the description for the post.</p>
         <span>Read more...</span>
+        <ul className="tag-list">
+          <li className="tag-default tag-pill tag-outline ">javascript</li>
+        </ul>
       </a>
     </div>
   </>
 );
 
-export default YourFeed;
+export default TagFeed;
---
+++ app/(feed)/feed/personal/page.tsx
@@ -0,0 +1,34 @@
+import type { FC } from 'react';
+
+// TODO: Complete YourFeed page
+const YourFeed: FC = () => (
+  <>
+    <div className="article-preview">
+      <div className="article-meta">
+        <a href="/">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img alt="user avatar" src="http://i.imgur.com/N4VcUeJ.jpg" />
+        </a>
+        <div className="info">
+          <a className="author" href="/">
+            Albert Pai
+          </a>
+          <span className="date">January 20th</span>
+        </div>
+        <button className="btn btn-outline-primary btn-sm pull-xs-right" type="button">
+          <i className="ion-heart"></i> 32
+        </button>
+      </div>
+      <a className="preview-link" href="/">
+        <h1>The song you will not ever stop singing. No matter how hard you try.</h1>
+        <p>This is the description for the post.</p>
+        <span>Read more...</span>
+        <ul className="tag-list">
+          <li className="tag-default tag-pill tag-outline ">implementations</li>
+        </ul>
+      </a>
+    </div>
+  </>
+);
+
+export default YourFeed;
