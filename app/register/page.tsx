9336449d2762aa2e94b4f2b8c7cea6edecdd845f
Dmitry Abramenko <dmitry.abramenko@madpaws.com.au>
2023-02-17 23:20:14 +0600

    feat(register): add user register functionality

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,39 +1,63 @@
+import { cookies } from 'next/headers';
 import Link from 'next/link';
-import type { FC } from 'react';
+import { redirect } from 'next/navigation';
+import type { ReactElement } from 'react';
+
+import { GLOBAL_FEED_ROUTE } from '~/common/constants';
+import type { User, UserResponse } from '~/common/types';
+import { RegisterForm } from '~/components/user/RegisterForm';
+
+// TODO: Extract the function to shared folder
+const getCurrentUser = async (): Promise<User | null> => {
+  const cookieStore = cookies();
+  const userTokenCookie = cookieStore.get('auth');
+
+  const hasUserToken = userTokenCookie && userTokenCookie.value;
+
+  if (!hasUserToken) {
+    return null;
+  }
+
+  // eslint-disable-next-line compat/compat
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/user`, {
+    headers: {
+      authorization: `Bearer ${userTokenCookie.value}`,
+    },
+    cache: 'no-store',
+  });
+
+  if (!response.ok) {
+    return null;
+  }
+
+  const currentUserResponse: UserResponse = await response.json();
+
+  return currentUserResponse.user;
+};
 
 // TODO: Complete Register page
-const Register: FC = () => (
-  <div className="auth-page">
-    <div className="container page">
-      <div className="row">
-        <div className="col-md-6 offset-md-3 col-xs-12">
-          <h1 className="text-xs-center">Sign up</h1>
-          <p className="text-xs-center">
-            <Link href="/login">Have an account?</Link>
-          </p>
+const Register = async (): Promise<ReactElement> => {
+  const user = await getCurrentUser();
 
-          <form>
-            <fieldset className="form-group">
-              <input className="form-control form-control-lg" placeholder="Your Name" type="text" />
-            </fieldset>
-            <fieldset className="form-group">
-              <input className="form-control form-control-lg" placeholder="Email" type="text" />
-            </fieldset>
-            <fieldset className="form-group">
-              <input
-                className="form-control form-control-lg"
-                placeholder="Password"
-                type="password"
-              />
-            </fieldset>
-            <button className="btn btn-lg btn-primary pull-xs-right" type="submit">
-              Sign up
-            </button>
-          </form>
+  if (user) {
+    redirect(GLOBAL_FEED_ROUTE);
+  }
+
+  return (
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign up</h1>
+            <p className="text-xs-center">
+              <Link href="/login">Have an account?</Link>
+            </p>
+            <RegisterForm />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Register;
---
+++ src/components/user/RegisterForm.tsx
@@ -0,0 +1,116 @@
+'use client';
+import { Formik } from 'formik';
+import type { FormikHelpers } from 'formik';
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation';
+import type { FormEvent, ReactElement } from 'react';
+import React, { useTransition } from 'react';
+
+import { INITIAL_USER_REGISTER_DATA } from './constants';
+import { parseAuthErrors } from './utils';
+
+import type { UserRegister } from '~/common/types';
+
+// TODO: Complete RegisterForm component
+export const RegisterForm = (): ReactElement => {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  // TODO: reimplement with httpOnly Cookies
+  const handleSubmitForm = async (
+    { username, email, password }: UserRegister,
+    { setErrors }: FormikHelpers<UserRegister>
+  ): Promise<void> => {
+    // eslint-disable-next-line compat/compat
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/users`, {
+      method: 'POST',
+      body: JSON.stringify({
+        user: { username, email, password },
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+
+      const parsedErrors = parseAuthErrors<UserRegister>(errorData.errors);
+      setErrors(parsedErrors);
+
+      return;
+    }
+
+    const userData = await response.json();
+
+    const {
+      user: { token },
+    } = userData;
+
+    Cookies.set('auth', token, {
+      expires: 1,
+      sameSite: 'strict',
+      secure: process.env.NODE_ENV === 'production',
+    });
+
+    startTransition(() => {
+      // Refresh the current route and fetch new data from the server without
+      // losing client-side browser or React state.
+      router.refresh();
+    });
+  };
+
+  // TODO: add client-side validation
+  return (
+    <Formik initialValues={INITIAL_USER_REGISTER_DATA} onSubmit={handleSubmitForm}>
+      {({ values, errors, handleChange, handleSubmit, isSubmitting }): ReactElement => (
+        <form
+          noValidate
+          onSubmit={(event: FormEvent<HTMLFormElement>): void => event.preventDefault()}
+        >
+          <fieldset className="form-group">
+            <input
+              className="form-control form-control-lg"
+              name="username"
+              onChange={handleChange}
+              placeholder="Your Name"
+              type="text"
+              value={values.username}
+            />
+            {errors.username && <div className="error-messages">{errors.username}</div>}
+          </fieldset>
+          <fieldset className="form-group">
+            <input
+              className="form-control form-control-lg"
+              name="email"
+              onChange={handleChange}
+              placeholder="Email"
+              type="text"
+              value={values.email}
+            />
+            {errors.email && <div className="error-messages">{errors.email}</div>}
+          </fieldset>
+          <fieldset className="form-group">
+            <input
+              className="form-control form-control-lg"
+              name="password"
+              onChange={handleChange}
+              placeholder="Password"
+              type="password"
+              value={values.password}
+            />
+            {errors.password && <div className="error-messages">{errors.password}</div>}
+          </fieldset>
+          <button
+            className="btn btn-lg btn-primary pull-xs-right"
+            disabled={isPending || isSubmitting}
+            onClick={(): void => handleSubmit()}
+            type="submit"
+          >
+            Sign up
+          </button>
+        </form>
+      )}
+    </Formik>
+  );
+};
